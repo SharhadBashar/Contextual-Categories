17e95fa73a4decfc34997f1f459e1d6e5088b259
Sharhad Bashar <sharhad.bashar@us0mcb009.targetspot.ny>
2023-10-18 10:15:42 -0400

    removed translation fo french topics

--- python/predict_iab.py
+++ python/predict_iab.py
@@ -36,8 +36,8 @@ class Predict_IAB:
 
 		recurring_n_words = self.get_recurring_n(text, n = RECURRING_N)
 		if (self.language != 'english'):
-			recurring_n_words = self.translate_words(recurring_n_words)
-		mapping = self.score_mapping(recurring_n_words, category_list, self.model_name)
+			recurring_n_words, recurring_n_words_original = self.translate_words(recurring_n_words)
+		mapping = self.score_mapping(recurring_n_words, category_list, self.model_name, recurring_n_words_original)
 		self.save_mapping(mapping, text_file, self.category_path, episode_id, show_id, self.language)
 
 	def get_custom_stopwords(self):
@@ -66,13 +66,14 @@ class Predict_IAB:
 		return Counter(text.split()).most_common(n)
 
 	def translate_words(self, recurring_n_words):
+		recurring_n_words_original = recurring_n_words.copy()
 		translator = Translator()
 		for i in range(RECURRING_N):
 			translation = translator.translate(recurring_n_words[i][0], src = self.language, dest = 'english').text
 			recurring_n_words[i] = (translation.lower(), recurring_n_words[i][1])
-		return recurring_n_words
+		return recurring_n_words, recurring_n_words_original
 
-	def score_mapping(self, recurring_n_words, category_list, model_name):
+	def score_mapping(self, recurring_n_words, category_list, model_name, recurring_n_words_original = []):
 		mapping = dict()
 		model = SentenceTransformer(model_name)
 		embs_word = torch.zeros(len(recurring_n_words), IAB_MODELS_PRAMS[model_name])
@@ -94,15 +95,24 @@ class Predict_IAB:
 				torch.save(embs_category, file)
 
 		scores, indices = torch.max(util.cos_sim(embs_word, embs_category), dim = -1)
-
-		for i, idx in enumerate(indices):
-			mapping[recurring_n_words[i][0]] = {
-				'id': category_list[idx][0],
-	 		    # 'data': recurring_n_words[i][0],
-	 		    # 'table': category_list[idx][1],
-	 		    'score': round(scores[i].item(), 4),
-	 		    'count': recurring_n_words[i][1]
-			}
+		if (self.language == 'english'):
+			for i, idx in enumerate(indices):
+				mapping[recurring_n_words[i][0]] = {
+					'id': category_list[idx][0],
+					# 'data': recurring_n_words[i][0],
+					# 'table': category_list[idx][1],
+					'score': round(scores[i].item(), 4),
+					'count': recurring_n_words[i][1]
+				}
+		else:
+			for i, idx in enumerate(indices):
+				mapping[recurring_n_words_original[i][0]] = {
+					'id': category_list[idx][0],
+					# 'data': recurring_n_words[i][0],
+					# 'table': category_list[idx][1],
+					'score': round(scores[i].item(), 4),
+					'count': recurring_n_words_original[i][1]
+				}
 		return mapping
 
 	def save_mapping(self, mapping, mapping_file, category_path, episode_id, show_id, language):
