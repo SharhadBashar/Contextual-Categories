f8fd5d30934c43382d741886cd49f5fda4a03fd5
Sharhad Bashar <sharhad.bashar@us0mcb009.targetspot.ny>
2023-12-13 12:46:04 -0500

    added custom categories to purge

--- python/purge.py
+++ python/purge.py
@@ -10,6 +10,7 @@ def delete():
     audio_files = glob(PATH_DATA_AUDIO + '*')
     category_files = glob(PATH_DATA_CATEGORY + '*')
     text_files = glob(PATH_DATA_TEXT + '*')
+    custom_category_files = glob(PATH_CUSTOM_TOPICS + '*')
     files = audio_files + category_files + text_files
     Logger(200, LOG_TYPE['i'], FILE_PRUGE_COUNT.format(len(files)))
     for file in tqdm(files):
