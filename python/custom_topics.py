ce300f67710b796f4d65736483bfb62100d7d700
Sharhad Bashar <sharhad.bashar@us0mcb009.targetspot.ny>
2023-12-13 12:15:11 -0500

    added custom topic search and result

--- python/custom_topics.py
+++ python/custom_topics.py
@@ -1,8 +1,12 @@
+import os
 import re
 import json
+import pickle
+from cleantext import clean
 import dateutil.parser as parser 
 from datetime import datetime, date
 
+from constants import *
 from database import Database
 
 class Custom_Topic:
@@ -36,7 +40,8 @@ class Custom_Topic:
         return keyword_score
 
 class Custom_Topics:
-    def __init__(self):
+    def __init__(self, text_data_path = None):
+        self.text_data_path = text_data_path if text_data_path else PATH_DATA_TEXT
         self.db = Database()
         
     def add_new_custom_topic(self, custom_topic: Custom_Topic):
@@ -49,5 +54,24 @@ class Custom_Topics:
                         ))
         self.db.write_custom_topic_keyword(values)
 
-    def find_custom_topic(self, custom_topic: Custom_Topic, text_file):
-        return True, {}
+    def clean_text(self, text_dict):
+        text = text_dict['text']
+        text = clean(text, clean_all = False,
+						   extra_spaces = True,
+						   stemming = False,
+						   stopwords = False,
+						   lowercase = True,
+						   numbers = False,
+						   punct = True
+					)
+        return text
+
+    def find_custom_topic(self, custom_topic, text_file):
+        podcast_score = 0
+        keyword_match = {}
+        text = self.clean_text(pickle.load(open(os.path.join(self.text_data_path, text_file), 'rb')))
+        for keyword, score in custom_topic['keyword']:
+            count = text.count(keyword)
+            keyword_match[keyword] = count
+            podcast_score += score * count
+        return podcast_score >= custom_topic['total_score'], keyword_match
